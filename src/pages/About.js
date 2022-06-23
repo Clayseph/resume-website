ee21ae9f32a27461f961f990329e95f879fbf40c
Clay <clayton.yarborough@gmail.com>
2022-06-23 10:21:01 -0400

    adds animations

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,39 @@
 import './About.css';
-
+import { useSpring, useTransition, animated, useTrail } from 'react-spring';
+import { experienceList } from '../resources/experience';
 export default function About(){
+
+    const move =  useSpring({
+        from: { marginTop:-200},
+        marginTop: 100,
+        config:{ duration: 1000 }
+    });
+
+
+
+    let trail = useTrail(experienceList.length, {
+        from: { opacity:0},
+        delay: 1000,
+        opacity: 1,
+        config:{ duration: 1500}
+    });
+
     return(
-        <div className="about-page">
-           
-            <h1>Professional Experience</h1>
-            <div className="experience-list">
-                <ul>
-                    <li>Slalom Consulting</li>
-                    <li>Home Depot</li>
-                    <li>AutoTrader</li>
-                </ul>
-            </div>
+        <div  className="about-page">
+
+                <animated.div style={move} >
+                    <div>
+                        <h1>Professional Experience</h1>
+                    </div>
+                </animated.div>
+
+                { trail.map(({...rest}, index)=>(
+                    <animated.div className="card" style={{ ...rest }}>
+                        <h2>{experienceList[index].company}</h2> 
+                        <p>{experienceList[index].experience}</p>
+                        </animated.div>
+                ))}
+               
         </div>
     );
 }
---
+++ src/resources/experience.js
@@ -0,0 +1,21 @@
+export const experienceList = [
+    { 
+        company: `Slalom Consulting`,
+        experience: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus at urna et egestas. Nunc lectus elit,
+        vestibulum eu tempus ut, dignissim et sapien. Nam volutpat porttitor ante, at faucibus lectus feugiat nec. Maecenas aliquet,
+        ligula non consectetur dictum, nunc dui dapibus mi, a aliquam lectus nibh sed nibh. Fusce non feugiat metus.
+        Vivamus finibus sit amet dolor quis efficitur. Suspendisse potenti.`},
+    {
+        company: `Home Depot`,
+        experience: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus at urna et egestas. Nunc lectus elit,
+        vestibulum eu tempus ut, dignissim et sapien. Nam volutpat porttitor ante, at faucibus lectus feugiat nec. Maecenas aliquet,
+        ligula non consectetur dictum, nunc dui dapibus mi, a aliquam lectus nibh sed nibh. Fusce non feugiat metus.
+        Vivamus finibus sit amet dolor quis efficitur. Suspendisse potenti.`},
+    {
+        company: `AutoTrader`,
+        experience: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus at urna et egestas. Nunc lectus elit,
+        vestibulum eu tempus ut, dignissim et sapien. Nam volutpat porttitor ante, at faucibus lectus feugiat nec. Maecenas aliquet,
+        ligula non consectetur dictum, nunc dui dapibus mi, a aliquam lectus nibh sed nibh. Fusce non feugiat metus.
+        Vivamus finibus sit amet dolor quis efficitur. Suspendisse potenti.`},
+    ]
+
