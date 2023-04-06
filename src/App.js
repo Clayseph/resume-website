c49eec981789e87ffa2287fc269e8292d4c3812a
Clay <clayton.yarborough@gmail.com>
2023-04-06 10:25:41 -0400

    adds linkedIn icon with active link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import 'react-pro-sidebar/dist/css/styles.css';
 import About from './pages/About';
 import SideBar from './components/Sidebar';
+import linkedInIcon from './LinkedIn.png'
 
 function App() {
   return (
@@ -11,12 +12,20 @@ function App() {
       <BrowserRouter >
 
         <SideBar/>
-
-        <Routes >
-          <Route path="/" element={<Home/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-        </Routes>
+        <div className="page">
+          <div className="content">
+            <Routes >
+              <Route path="/" element={<Home/>}/>
+              <Route path="/home" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+            </Routes>
+          </div>
+          <div className="footer">
+            <a href="https://www.linkedin.com/in/clayton-yarborough-3038bb68/" target="_blank">
+              <img className="icon" src={linkedInIcon}/>
+            </a>
+          </div>
+        </div>
       </BrowserRouter>
     </div>
   );
